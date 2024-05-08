1678a7b6ab098df1cad06e0ce856fd6be3683efd
Ashutosh <ashutoshtips@gmail.com>
2024-05-08 21:40:04 +0530

    Update convert-apt-to-nala.sh
    Rework some Logic and more fixes..

--- convert-apt-to-nala.sh
+++ convert-apt-to-nala.sh
@@ -3,11 +3,6 @@ echo " "
 echo "Convert Apt to Nala"
 echo " By @its_ashu_otf "
 echo " "
-echo "Adding Repository"
-echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
-echo " "
-echo "Done !"
-echo " "
 echo "Installing Nala......"
 echo " "
 sudo apt update && sudo apt install nala
@@ -19,32 +14,33 @@ sudo nala fetch
 echo " "
 echo "Adding Nala to .zshrc to convert APT to Nala"
 
-sed -i '$ a\
-\napt() { \
-\n  command nala "$@" \
-\n} \
-\nsudo() { \
-\n  if [ "$1" = "apt" ]; then \
-\n    shift \
-\n    command sudo nala "$@" \
-\n  else \
-\n    command sudo "$@" \
-\n  fi \
-\n}' ~/.zshrc
+echo 'apt() { 
+  command nala "$@"
+}
+sudo() {
+  if [ "$1" = "apt" ]; then
+    shift
+    command sudo nala "$@"
+  else
+    command sudo "$@"
+  fi
+}' >> ~/.zshrc
+
 echo " "
 echo "Adding Nala to .bashrc to convert APT to Nala"
-sed -i '$ a\
-\napt() { \
-\n  command nala "$@" \
-\n} \
-\nsudo() { \
-\n  if [ "$1" = "apt" ]; then \
-\n    shift \
-\n    command sudo nala "$@" \
-\n  else \
-\n    command sudo "$@" \
-\n  fi \
-\n}' ~/.bashrc
+
+echo 'apt() { 
+  command nala "$@"
+}
+sudo() {
+  if [ "$1" = "apt" ]; then
+    shift
+    command sudo nala "$@"
+  else
+    command sudo "$@"
+  fi
+}' >> ~/.bashrc
+
 
 echo " "
 echo "Sourcing the zshrc & bashrc file"
