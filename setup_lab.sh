c302475008b9e276e051f436abd58f877198f441
Ashutosh <ashutoshtips@gmail.com>
2024-06-11 01:25:36 +0530

    Update setup_lab.sh

--- setup_lab.sh
+++ setup_lab.sh
@@ -3,6 +3,20 @@
 echo "Installing ZSH Profile...."
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/its-ashu-otf/myZSH/main/setup.sh)"
 
+# Check if running with sudo privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "This script needs to be run with sudo privileges."
+    read -p "Do you want to escalate? (y/n): " choice
+    if [ "$choice" == "y" ]; then
+        sudo "$0" "$@"  # Execute this script with sudo
+    else
+        echo "Script execution aborted."
+        exit 1
+    fi
+fi
+
+echo "Running with sudo privileges!"
+
 echo "Installing Floorp..."
 curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
@@ -19,5 +33,11 @@ wget -q --show-progress https://raw.githubusercontent.com/its-ashu-otf/Fix-my-Ka
 chmod +x snapd_fixer.sh
 sudo ./snapd_fixer.sh
 rm snapd_fixer.sh
+
+echo "Fixing MetaSploit APK Singing issues..."
+wget -q --show-progress https://raw.githubusercontent.com/its-hritika/Kali-Fix/main/metasploit_fix.sh
+chmod +x metasploit_fix.sh
+sudo ./metasploit_fix.sh
+rm metasploit_fix.sh
 
 echo "Done ! Reboot you're System to see the Changes..."
